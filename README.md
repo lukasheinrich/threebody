14945439a9225df3a4a38824f7bc97a60f12200f
Lukas Heinrich <lukas.heinrich@gmail.com>
2014-08-16 17:18:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+SUSY Three Body Decays
+======================
+
+
+This is code for an upcoming analysis of ATLAS data looking for supersymmetric decays.
---
+++ workflow/rivetanalysis/RivetStops.cxx
@@ -0,0 +1,38 @@
+#include "Rivet/Analysis.hh"
+#include "Rivet/Projections/MissingMomentum.hh"
+#include "Rivet/AnalysisLoader.hh"
+
+namespace Rivet {
+  
+  class StopThreeBody : public Analysis {  
+  public:
+    
+    /// Default constructor
+    StopThreeBody() : Analysis("StopThreeBody") { }    
+    
+    
+    /// @name Analysis methods
+    //@{
+    void init() { 
+      const FinalState cnfs;
+			addProjection(MissingMomentum(cnfs), "ETmiss");
+			_histMET    = bookHisto1D("MET", 100, 0, 3000);
+    }
+    
+    void analyze(const Event& event) {
+			const double weight = event.weight();
+			const MissingMomentum& met = applyProjection<MissingMomentum>(event, "ETmiss");
+			_histMET->fill(met.scalarEt(),weight);
+    }
+    
+		void finalize(){
+      MSG_DEBUG("Finalize");			
+			scale(_histMET, 1/sumOfWeights());
+		}
+
+	private:
+		Histo1DPtr _histMET;
+  };
+
+  DECLARE_RIVET_PLUGIN(StopThreeBody);
+}
