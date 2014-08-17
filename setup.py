3d9d919e3f4ad66f2eeb1ac82eb2016f9f8a12bf
Lukas Heinrich <lukas.heinrich@gmail.com>
2014-08-16 21:16:30 -0400

    hepmc reader

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+      name = 'threebody',
+      version = 0.1,
+      packages = find_packages(),
+      author = 'Lukas Heinrich',
+      author_email = 'lukas.heinrich@cern.ch',
+      scripts=['utils/readhepmc.py'],
+      )
---
+++ utils/readhepmc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from hepmcanalysis.events import events
+from hepmcanalysis.streamproxy import ifstream_proxy
+import hepmc
+import pypdt
+import argparse
+import IPython
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f','--file',dest='file',type=str,help='input HepMC file')
+
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(1)
+
+args = parser.parse_args()
+
+def main():
+  proxy = ifstream_proxy(args.file)
+
+  g = hepmc.IO_GenEvent(proxy.stream())
+  for i,e in enumerate(events(g)):
+    print '----------------'
+    finalstate = [p for p in e.particles() if p.status()==1]
+    print 'event #{}: number of particles in event: {}'.format(i,len(finalstate))
+    
+if __name__ == '__main__':
+  main()
