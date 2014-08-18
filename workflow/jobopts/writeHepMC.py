8a6886f50f4fd82047b610d8cfbc6f0ea9af582f
Lukas Alexander Heinrich <lheinric@lxplus0182.cern.ch>
2014-08-18 04:19:58 +0200

    hepmc jobops

---
+++ workflow/jobopts/writeHepMC.py
@@ -0,0 +1,32 @@
+from AthenaCommon.AppMgr import ServiceMgr
+import AthenaPoolCnvSvc.ReadAthenaPool
+ServiceMgr.EventSelector.InputCollections = ["evgen.pool.root"]
+
+from AthenaCommon.AlgSequence import AlgSequence
+job = AlgSequence()
+
+from McParticleTools.McParticleToolsConf import HepMcWriterTool
+from McParticleAlgs.McParticleAlgsConf   import GenEventAsciiWriter
+McWriter = HepMcWriterTool(McEvents="GEN_EVENT", Output="events.hepmc")
+job += GenEventAsciiWriter(McWriter=McWriter, OutputLevel=INFO)
+
+theApp.EvtMax = 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
