c1cb68733a0abeb75e3d01c930f141f5d7ab0b65
lukasheinrich <lukas.heinrich@gmail.com>
2014-08-22 13:26:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
       author_email = 'lukas.heinrich@cern.ch',
       scripts=['utils/readhepmc.py','utils/jj2.py'],
       install_requires=['pyhepmc>=0.5','mcviz','hepmcanalysis','pypdt','jinja2'],
-      dependency_links=['https://github.com/mcviz/mcviz/tarball/master#egg=mcviz',
-                        'https://github.com/lukasheinrich/hepmcanalysis/tarball/master#egg=hepmcanalysis']
+      dependency_links=['https://github.com/mcviz/mcviz/tarball/master#egg=mcviz-0.1',
+                        'https://github.com/lukasheinrich/hepmcanalysis/tarball/master#egg=hepmcanalysis=0.0.1']
       )
